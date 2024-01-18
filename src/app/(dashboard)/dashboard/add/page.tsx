7daca7c1c56c544aa98568e16f328c96400ad839
MaxParastiuk <Parastukm@gmail.com>
2024-01-18 11:03:54 +0100

    test production

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -5,7 +5,7 @@ const Page: FC = () => {
   return (
     <main className="pt-8">
       <h1 className=" font-bold text-5xl mb-8">Add a friend</h1>
-      <AddFriendButton />
+      {/* <AddFriendButton /> */}
     </main>
   );
 };
