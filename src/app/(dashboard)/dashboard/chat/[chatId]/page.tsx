3c82b5359ff2f00f43a64cb7130059f1a674c55c
MaxParastiuk <Parastukm@gmail.com>
2024-01-18 12:43:50 +0100

    fix

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -8,13 +8,13 @@ import { getServerSession } from 'next-auth';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
-interface PageProps {
+interface pageProps {
   params: {
     chatId: string;
   };
 }
 
-async function getChatMessages(chatId: string) {
+async function getMessages(chatId: string) {
   try {
     const results: string[] = await fetchRedis(
       'zrange',
@@ -22,75 +22,68 @@ async function getChatMessages(chatId: string) {
       0,
       -1,
     );
-
     const dbMessages = results.map((message) => JSON.parse(message) as Message);
-
-    const reversedDbMessages = dbMessages.reverse();
-
-    const messages = messageArrayValidator.parse(reversedDbMessages);
-
+    const revesedDbMessages = dbMessages.reverse();
+    const messages = messageArrayValidator.parse(revesedDbMessages);
     return messages;
   } catch (error) {
-    notFound();
+    console.log('Error => ', error);
   }
 }
 
-const page = async ({ params }: PageProps) => {
+const page = async ({ params }: pageProps) => {
   const { chatId } = params;
 
   const session = await getServerSession(authOptions);
-
-  if (!session) notFound();
-
-  const { user } = session;
-
-  const [userId1, userId2] = chatId.split('--');
-
-  if (user.id !== userId1 && user.id !== userId2) {
+  if (!session) {
     notFound();
   }
 
-  const chatPartnerId = user.id === userId1 ? userId2 : userId1;
+  const { user } = session;
+  const [userId1, userid2] = chatId.split('--');
 
+  if (user.id !== userId1 && user.id !== userid2) {
+    notFound();
+  }
+
+  const chatPartnerId = user.id === userId1 ? userid2 : userId1;
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
+  const initialMesages = (await getMessages(chatId)) as Message[];
 
-  const initialMessages = await getChatMessages(chatId);
-
+  // mock a delay of 2000 ms
   return (
-    <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]">
-      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200 ">
+    <div className="flex flex-col justify-between flex-1 h-full max-h-[calc(100vh-6rem)]">
+      <div className="flex justify-between py-3 border-b-2 border-gray-200 sm:items-center">
         <div className="relative flex items-center space-x-4">
           <div className="relative">
-            <div className="relative w-8 sm:w-12 h-8 sm:h-12">
+            <div className="relative w-8 h-8 sm:w-12 sm:h-12">
               <Image
                 fill
                 referrerPolicy="no-referrer"
                 src={chatPartner.image}
-                alt={`${chatPartner.name} profile picture`}
-                className="rounded-full"
-              ></Image>
+                alt={`${chatPartner.name}'s profile picture`}
+                className="rounded-full "
+              />
             </div>
           </div>
-
           <div className="flex flex-col leading-tight">
-            <div className="text-xl flex items-center">
-              <span className="text-gray-700 mr-3 font-semibold">
+            <div className="flex items-center text-xl">
+              <span className="mr-3 font-semibold text-gray-700">
                 {chatPartner.name}
               </span>
             </div>
-
-            <span className="text-sm text-gray-600 ">{chatPartner.email}</span>
+            <span className="text-sm to-gray-600">{chatPartner.email}</span>
           </div>
         </div>
       </div>
       <Messages
+        initialMessages={initialMesages}
         chatId={chatId}
-        chatPartner={chatPartner}
-        sessionImg={session.user.image}
         sessionId={session.user.id}
-        initialMessages={initialMessages}
+        sessionImg={session.user.image}
+        chatPartner={chatPartner}
       />
-      <ChatInput chatPartner={chatPartner} chatId={chatId} />
+      <ChatInput chatId={chatId} chatPartner={chatPartner} />
     </div>
   );
 };
